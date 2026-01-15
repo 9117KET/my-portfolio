4689920d4a0bcf07052ac9c8489d2ae00e398c79
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2026-01-15 16:19:42 +0100

    updated eevnt with bmw hackathon

--- src/pages/Misc.tsx
+++ src/pages/Misc.tsx
@@ -5,12 +5,12 @@ import styles from "../utils/style";
 import { certificates, awards, events } from "../utils/constants";
 import { Event } from "../types/event";
 
-const categories = ["Certificates", "Awards", "Events"];
+const categories = ["Events", "Certificates", "Awards"];
 
 const itemsToShow = {
+  Events: events,
   Certificates: certificates,
   Awards: awards,
-  Events: events,
 };
 
 type Category = keyof typeof itemsToShow;
